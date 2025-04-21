a636d9aae2af2ab2205866fb2bbbaa9ceef4180e
sung23092003 <hsung230903@gmail.com>
2025-04-21 15:17:28 +0700

    custom hook useDebounce

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -5,6 +5,7 @@ import HeadlessTippy from '@tippyjs/react/headless';
 import classNames from 'classnames/bind';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
+import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
@@ -14,10 +15,12 @@ const Search = () => {
     const [showResult, setShowResult] = useState(true);
     const [loading, setLoading] = useState(false);
 
+    const debounced  = useDebounce(searchValue,500)
+
     const inputRef = useRef();
     useEffect(() => {
         // loại bỏ khoảng trắng và ktr nếu searchValue ko có giá trị thì return
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setsearchResult([]);
             return;
         }
@@ -25,7 +28,7 @@ const Search = () => {
         setLoading(true);
         // Khi người dùng nhập các ký tự đặc biệt như `&`,'=', `?` có thể khiến URL bị hiểu sai,
         // hàm encodeURIComponent sẽ mã hóa chúng thành các ký tự an toàn để sử dụng trong URL
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then((res) => res.json())
             .then((res) => {
                 setsearchResult(res.data);
@@ -35,7 +38,7 @@ const Search = () => {
                 console.error('Fetch error:', error);
                 setLoading(false);
             });
-    }, [searchValue]);
+    }, [debounced]);
 
     const handleClear = () => {
         setSearchValue('');
